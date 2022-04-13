c069700580b7946cf7e80604d4e5d55734f9caaf
KuVaM-cOdEr <103585713+KuVaM-cOdEr@users.noreply.github.com>
2022-04-13 20:23:27 +0530

    Add files via upload

---
+++ lab assignment 1 q1.py
@@ -0,0 +1,6 @@
+print("Write a python program to find average of three numbers entered by the user.")
+a=int(input("Enter First No.="))
+b=int(input("Enter Second No.="))
+c=int(input("Enter Third No.="))
+d=(a+b+c)/3
+print("The average of three number is",d)
